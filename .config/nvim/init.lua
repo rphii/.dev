f0f57f16cf9d0034b370e88ae304b6c0cec480aa
rphii <raphael.seitz18@gmail.com>
2026-08-28 17:01:39 +0200

    add back the hl fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -539,3 +539,23 @@ vim.api.nvim_set_hl(0, "LspReferenceText", {
   bold = true,
 })
 
+
+-- Fix highlighting for floating window..
+local set_hl_for_floating_window = function()
+  vim.api.nvim_set_hl(0, 'NormalFloat', {
+    link = 'Normal',
+  })
+  vim.api.nvim_set_hl(0, 'FloatBorder', {
+    bg = 'none',
+  })
+  vim.api.nvim_set_hl(0, "Pmenu", { link = 'Normal', }) -- menu background
+end
+
+set_hl_for_floating_window()
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  desc = 'Avoid overwritten by loading color schemes later',
+  callback = set_hl_for_floating_window,
+})
+
